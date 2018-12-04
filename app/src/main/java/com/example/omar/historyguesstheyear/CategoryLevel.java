dce98192da243dd2095c29fee3ff27921dd4f950
Omar Samara <omsamara@ncsu.edu>
2018-12-03 21:46:56 -0500

    Added enum support for category levels

---
+++ app/src/main/java/com/example/omar/historyguesstheyear/CategoryLevel.java
@@ -0,0 +1,23 @@
+package com.example.omar.historyguesstheyear;
+
+/**
+ * Created by Omar on 12/3/2018.
+ */
+
+public enum CategoryLevel {
+
+    /** Level for the player begins at. Any amount sets the level to novice */
+    NOVICE,
+
+    /** Level for a combined total of less than 50 years */
+    INTERMEDIATE,
+
+    /** Level for a combined total of less than 25 years */
+    EXPERT,
+
+    /** Level for a combined total of less than 10 years */
+    HISTORY_MASTER,
+
+    /** Level for a combined total of 0 years off. A true master. */
+    HISTORIAN_GOD;
+}
--- app/src/main/java/com/example/omar/historyguesstheyear/MainActivity.java
+++ app/src/main/java/com/example/omar/historyguesstheyear/MainActivity.java
@@ -5,9 +5,24 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Mastery level tracker
+    private CategoryLevel industrialLevel;
+    private CategoryLevel civilRightsLevel;
+    private CategoryLevel progressiveLevel;
+
+
+    /**
+     * Entry point for the app
+     *
+     * @param savedInstanceState saved information used for the restoration of the activity.
+     *                           Initially null.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Instantiate all class variables
+        
     }
 }
