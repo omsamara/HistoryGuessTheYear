9cfc5d65154b9e32981212d21b981a938fdb24d5
Omar Samara <omsamara@ncsu.edu>
2018-12-04 13:43:15 -0500

    Added necessary views and strings for levels and buttons

--- app/src/main/java/com/example/omar/historyguesstheyear/MainActivity.java
+++ app/src/main/java/com/example/omar/historyguesstheyear/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.omar.historyguesstheyear;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +12,10 @@ public class MainActivity extends AppCompatActivity {
     private CategoryLevel civilRightsLevel;
     private CategoryLevel progressiveLevel;
 
+    // Category Buttons
+    private Button industrialButton;
+    private Button civilRightsButton;
+    private Button progressiveButton;
 
     /**
      * Entry point for the app
@@ -23,6 +29,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Instantiate all class variables
-        
+
+    }
+
+    private void instantiateButtoWidths() {
+
+        industrialButton = findViewById(R.id.industrialButton);
+        civilRightsButton = findViewById(R.id.civilRightsButton);
+        progressiveButton = findViewById(R.id.progressiveButton);
+
+        int width = getResources().getDisplayMetrics().widthPixels / 3;
+        int height = getResources().getDisplayMetrics().heightPixels / 10;
+        industrialButton.setLayoutParams(new RelativeLayout.LayoutParams(width, height));
     }
 }
